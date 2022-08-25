65bce279be9cde17785d4374c4e3a96162c9bf75
alex <kotzspam@gmail.com>
2022-08-25 14:31:55 +0200

    Add popular movie getter

--- tmdb/src/App.jsx
+++ tmdb/src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import "./assets/scss/App.scss";
+import PopularMovies from "./pages/PopularMovies";
 
 function App() {
 	return (
@@ -11,7 +12,7 @@ function App() {
 
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				<Route path="/movies" element={<NotFound />} />
+				<Route path="/popular" element={<PopularMovies />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
--- tmdb/src/services/TmdbApi.js
+++ tmdb/src/services/TmdbApi.js
@@ -2,3 +2,14 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+
+// Get popular movies
+const getPopularMovies = async () => {
+	return await axios.get(`movie/popular?api_key=${API_KEY}`);
+};
+
+const exports = {
+	getPopularMovies,
+};
+
+export default exports;
