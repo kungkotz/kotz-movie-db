ace956a8539aa4e71a899218345857c8f4a33d90
alex <kotzspam@gmail.com>
2022-08-25 14:10:05 +0200

    Add new navlinks

--- tmdb/src/components/Navigation.jsx
+++ tmdb/src/components/Navigation.jsx
@@ -1,23 +1,34 @@
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { Link, NavLink } from 'react-router-dom'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand as={Link} to="/">React Template</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">
+					{" "}
+					Kotz Movie Database
+				</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
+						<Nav.Link as={NavLink} end to="/popular">
+							Popular
+						</Nav.Link>
+						<Nav.Link as={NavLink} end to="/toplist">
+							Toplist
+						</Nav.Link>
+						<Nav.Link as={NavLink} end to="/current">
+							In Cinema Now
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-	)
-}
+	);
+};
 
-export default Navigation
+export default Navigation;
