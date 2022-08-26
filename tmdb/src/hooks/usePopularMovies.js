7d34da29847a806a1863b4448df113b13d1a12d0
alex <kotzspam@gmail.com>
2022-08-27 00:38:57 +0200

    temp removed custom hook

--- tmdb/src/hooks/usePopularMovies.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import TmdbApi from "../services/TmdbApi";
-
-const usePopularMovies = () => {
-	return useQuery("popularMovies", TmdbApi.getPopularMovies);
-};
-
-export default usePopularMovies;
