f2f27c2dbd7c88be6b3e723455a21d86fd44eb13
alex <kotzspam@gmail.com>
2022-08-28 21:08:30 +0200

    add html into moviecard

---
+++ tmdb/src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/button";
+import { Link } from "react-router-dom";
+
+const MovieCard = ({ data, imgLink }) => {
+	return (
+		<div className="col-lg-2">
+			<Card className="">
+				<Card.Img variant="top" src={imgLink + data.poster_path} />
+				<Card.Body className="text-center">
+					<Card.Title className="overflow-auto movies-overflow text-center">
+						{data.title}
+					</Card.Title>
+
+					<Button as={Link} to={`/movie/${data.id}`} variant="primary">
+						Read more
+					</Button>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
+
+export default MovieCard;
--- tmdb/src/pages/PopularMovies.jsx
+++ tmdb/src/pages/PopularMovies.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { Link } from "react-router-dom";
+
 import { getPopularMovies } from "../services/api";
 import { useQuery } from "react-query";
-import Button from "react-bootstrap/button";
+
 import LoadingSpinner from "../components/LoadingSpinner";
+
+import CardGroup from "react-bootstrap/CardGroup";
+import MovieCard from "../components/MovieCard";
 
 const imgLink = "https://image.tmdb.org/t/p/w500";
 
@@ -18,21 +21,12 @@ const PopularMovies = () => {
 			<h1 className="text-center">Popular Movies</h1>
 			{isLoading && <LoadingSpinner />}
 			{isError && <p>An error occured: {error.message}</p>}
-			{data &&
-				data.results.map((movie) => (
-					<div
-						key={movie.id}
-						className="d-flex justify-content-center main-container"
-					>
-						<div className="card m-4 text-center">
-							<h1 className="card-header text-dark">{movie.title}</h1>
-							<img src={imgLink + movie.poster_path} alt="" />
-							<Button as={Link} to={`/movie/${movie.id}`} variant="primary">
-								Read more
-							</Button>
-						</div>
-					</div>
-				))}
+			<CardGroup>
+				{data &&
+					data.results.map((movie) => (
+						<MovieCard data={movie} imgLink={imgLink} />
+					))}
+			</CardGroup>
 		</Container>
 	);
 };
